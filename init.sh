86dbf84d15f5d15f93a24ad3241d70942039fa77
lupujo <yoni@lupu.co.il>
2020-03-17 14:34:55 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -12,3 +12,15 @@ chown -R $HOSTUID:$HOSTGID /$MAINFOLDER/env1 /$MAINFOLDER/env2 /$MAINFOLDER/env3
 chown -R $ENV1UID:$ENV1GID /$MAINFOLDER/env1/www
 chown -R $ENV2UID:$ENV2GID /$MAINFOLDER/env2/www
 chown -R $ENV3UID:$ENV3GID /$MAINFOLDER/env3/www
+if [ ! -f "$MAINFOLDER/env1/ssl/ssl.key" ]; then
+	mkdir -p $MAINFOLDER/env1/ssl
+	cp ssl/* $MAINFOLDER/env1/ssl/
+fi
+if [ ! -f "$MAINFOLDER/env2/ssl/ssl.key" ]; then
+	mkdir -p $MAINFOLDER/env2/ssl
+	cp ssl/* $MAINFOLDER/env2/ssl/
+fi
+if [ ! -f "$MAINFOLDER/env3/ssl/ssl.key" ]; then
+	mkdir -p $MAINFOLDER/env3/ssl
+	cp ssl/* $MAINFOLDER/env3/ssl/
+fi
