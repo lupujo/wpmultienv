9b5e537803071026b89237b2f56d1762fdc48966
lupujo <yoni@lupu.co.il>
2020-04-20 16:28:28 +0300

    Update deploy.sh

--- wp/deploy.sh
+++ wp/deploy.sh
@@ -65,7 +65,9 @@ mysql -uroot -p${SQLPASSROOT} -hdb wordpress < /wpmultienv/sql
 echo "Importing files..."
 mkdir -p /var/www/html-old
 mv /var/www/html/* /var/www/html-old/
+mv /var/www/html/.* /var/www/html-old/
 mv /wpmultienv/www/* /var/www/html/
+mv /wpmultienv/www/.* /var/www/html/
 
 echo "Running search and replace from $OLDWWWHOST to $WWWHOST via Migrate DB plugin"
 wp migratedb find-replace --find=$OLDWWWHOST --replace=$WWWHOST
