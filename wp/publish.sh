8b0db72d7ae0fff463619504632fc2b0fdaf5245
lupujo <yoni@lupu.co.il>
2020-10-12 19:34:03 +0300

    Update publish.sh

--- wp/publish.sh
+++ wp/publish.sh
@@ -19,7 +19,7 @@ echo $WWWHOST > /var/www/html/wpmultienv/wwwhost
 echo "Compressing files..."
 rm -f /wpmultienv/files.tbz2 >/dev/null 2>&1
 cd /var/www/html/
-tar cjvf /wpmultienv/files.tbz2 * >/dev/null 2>&1
+tar cjvf /wpmultienv/files.tbz2 --exclude='wpmultienv/rewrites-env.conf' * >/dev/null 2>&1
 
 echo "Uploading to S3..."
 AWS_ACCESS_KEY_ID=$S3KEY AWS_SECRET_ACCESS_KEY=$S3SECRET aws s3 mv /wpmultienv/files.tbz2 s3://$S3BUCKET/$TAG-files.tbz2
